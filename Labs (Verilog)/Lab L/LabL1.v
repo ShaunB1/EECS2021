fbe8cb6c3138709145d7f3e9d49d64e3ad0ac163
ShaunB1 <shaunbautista1@gmail.com>
2023-03-19 16:39:23 -0400

    Modified LabL1

--- Labs (Verilog)/Lab L/LabL1.v
+++ Labs (Verilog)/Lab L/LabL1.v
@@ -1,18 +1,3 @@
-module yMux1(z, a, b, c);
-
-output z;
-input a, b, c;
-integer i, j, k;
-
-wire notC, upper, lower;
-
-not my_not(notC, c);
-and upperAnd(upper, a, notC);
-and lowerAnd(lower, c, b);
-or my_or(z, upper, lower);
-
-endmodule
-
 module LabL1;
 
 reg a, b, c, expect;
