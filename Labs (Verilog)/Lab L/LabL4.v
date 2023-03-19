b1edc9a254e5b2420b7d12bfc6f7e90eb1530730
ShaunB1 <shaunbautista1@gmail.com>
2023-03-19 19:27:41 -0400

    LabL4

---
+++ Labs (Verilog)/Lab L/LabL4.v
@@ -0,0 +1,45 @@
+module LabL4;
+
+reg[31:0] a0, a1, a2, a3, expect;
+reg[1:0] c;
+wire[31:0] z;
+
+yMux4to1 #(.SIZE(32)) mux(z, a0, a1, a2, a3, c);
+
+initial begin
+    
+    repeat(10) begin
+
+        a0 = $random;
+        a1 = $random;
+        a2 = $random;
+        a3 = $random;
+        c = $random % 4;
+
+        if(c === 0) begin
+            expect = a0;
+        end
+
+        else if(c === 1) begin
+            expect = a1;
+        end
+
+        else if(c === 2) begin
+            expect = a2;
+        end
+
+        else begin
+            expect = a3;
+        end
+
+        if(expect != z) begin
+            $display("FAIL: a0 = %b a1 = %b a2 = %b  a3 = %b c = %b z = %b", a0, a1, a2, a3, c, z);
+        end
+
+    end
+
+    $finish;
+
+end
+
+endmodule;
