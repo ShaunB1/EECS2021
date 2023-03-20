2556563e7a4f9eeb67f4dd3298744bf119eb559e
ShaunB1 <shaunbautista1@gmail.com>
2023-03-19 20:12:16 -0400

    LabL5.v

---
+++ Labs (Verilog)/Lab L/yAdder1.v
@@ -0,0 +1,12 @@
+module yAdder1(z, cout, a, b, cin);
+
+input a, b, cin;
+output z, cout;
+
+xor xor1(upper1, a, b);
+xor xor2(z, cin, upper1);
+and and1(lower1, a, b);
+and and2(lower2, upper1, cin);
+or or1(cout, lower2, lower1);
+
+endmodule;
