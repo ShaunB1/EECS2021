7102685b1faf9521b120be10fb2aadd22b9aca04
ShaunB1 <shaunbautista1@gmail.com>
2023-03-21 16:20:19 -0400

    LabL11

---
+++ Labs (Verilog)/Lab L/LabL11.v
@@ -0,0 +1,133 @@
+module LabL11;
+
+    reg signed[31:0] a, b, expect;
+    reg[2:0] op;
+    wire ex;
+    wire [31:0] z;
+    reg ok = 0, flag, tmp, zero;
+    reg [1*7:0] operator;
+
+    yAlu alu(z, ex, a, b, op);
+
+    initial begin
+        
+        repeat(100) begin
+
+            a = $random;
+            b = $random;
+            //flag = $value$plusargs("op = %d", op);
+            op = $random%7;
+            tmp = $random % 2;
+
+            zero = (expect == 0) ? 1 : 0;
+
+            // The oracle: computer "expect" based on "op"
+            // Compare the circuit's output with "expect"
+            // and set "ok" accordingly
+            // Display ok and the various signals
+
+            #1;
+
+            if(tmp === 0) begin
+                b = a;
+            end
+
+            #1;
+
+            if(op === 3'b000) begin // op = 0
+                
+                expect = a & b;
+                operator = "&";
+
+                #1;
+                ok = (expect === z) ? 1 : 0;
+
+                if(ok) begin
+                    $display("PASS (AND): a = %b b = %b a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+
+                else begin
+                    $display("FAIL (AND): a = %b b = %b a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+                
+            end
+
+            else if(op == 3'b001) begin // op = 1
+                
+                expect = a | b;
+                operator = "|";
+                
+                #1;
+                ok = (expect === z) ? 1 : 0;
+
+                if(ok) begin
+                    $display("PASS (OR): a = %b b = %b a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+
+                else begin
+                    $display("FAIL (OR): a = %b b = %b a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+
+            end
+
+            else if(op == 3'b010) begin // op = 2
+                
+                expect = a + b;
+                operator = "+";
+
+                #1;
+                ok = (ok) ? 1 : 0;
+
+                if(expect === z) begin
+                    $display("PASS (ADD): a = %b b = %b a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+                
+                else begin
+                    $display("FAIL (ADD): a = %b b = %b a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+
+            end
+
+            else if(op == 3'b110) begin // op = 6
+
+                expect = a - b;
+                operator = "-";
+
+                #1;
+                ok = (expect === z) ? 1 : 0;
+
+                if(ok) begin
+                    $display("PASS (SUB): a = %b b = %b a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+
+                else begin
+                    $display("FAIL (SUB): a = %b b = %b a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+                
+            end
+
+            else if(op == 3'b111) begin // op = 7
+                
+                expect = (a < b) ? 1 : 0;
+                operator = "<";
+
+                #1;
+                ok = (expect === z) ? 1 : 0;
+
+                if(ok) begin
+                    $display("PASS (<): a = %d b = %d a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+
+                else begin
+                    $display("FAIL (<): a = %d b = %d a %s b = %b zero = %b", a, b, operator, z, zero);
+                end
+
+            end
+
+        end
+
+        $finish;
+
+    end
+
+endmodule
--- Labs (Verilog)/Lab L/yAlu.v
+++ Labs (Verilog)/Lab L/yAlu.v
@@ -6,16 +6,30 @@ module yAlu(z, ex, a, b, op);
     output ex;
     wire [31:0] alu_and, alu_or, alu_arith, slt, tmp;
     wire cout;
+    
+    wire [15:0] z16;
+    wire [7:0] z8;
+    wire [3:0] z4;
+    wire [1:0] z2;
+    wire z1, z0;
 
     assign slt[31:1] = 0; // upper bits are always 0
     assign ex = 0; // not supported for now
+
+    or or16[15:0](z16, z[15:0], z[31:16]);
+    or or8[7:0](z8, z16[7:0], z16[15:8]);
+    or or4[3:0](z4, z8[3:0], z8[7:4]);
+    or or2[1:0](z2, z4[1:0], z4[3:2]);
+    or or1(z1, z2[0], z2[1]);
+    not ex_not(z0, z1);
+    assign ex = z0;
 
     xor(condition, a[31], b[31]);
     assign tmp = a - b;
     yMux #(.SIZE(1)) slt_mux(slt[0], tmp[31], a[31], condition);
     
     and and1[31:0](alu_and, a, b);
-    or or1[31:0](alu_or, a, b);
+    or my_or[31:0](alu_or, a, b);
     yArith arith1[31:0](alu_arith, cout, a, b, op[2]);
     yMux4to1 #(.SIZE(32)) mux1(z, alu_and, alu_or, alu_arith, slt, op[1:0]);
 
