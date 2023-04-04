4c277812f238b7350f89a72feb96622cdeab75ba
ShaunB1 <shaunbautista1@gmail.com>
2023-04-03 22:05:31 -0400

    LabM8

--- Labs (Verilog)/Lab M/cpu.v
+++ Labs (Verilog)/Lab M/cpu.v
@@ -140,3 +140,56 @@ module yIF(ins, PCp4, PCin, clk);
     mem my_mem(ins, regOut, , clk, 1'b1, 1'b0);
 
 endmodule
+
+module yID(rd1, rd2, immOut, jTarget, branch, ins, wd, RegWrite, clk);
+    output [31:0] rd1, rd2, immOut;
+    output [31:0] jTarget;
+    output [31:0] branch;
+    input [31:0] ins, wd;
+    input RegWrite, clk;
+    wire [19:0] zeros, ones; // For I-Type and SB-Type
+    wire [11:0] zerosj, onesj; // For UJ-Type
+    wire [31:0] imm, saveImm; // For S-Type
+    
+    rf myRF(rd1, rd2, ins[19:15], ins[24:20], ins[11:7], wd, clk,
+    RegWrite);
+    assign imm[11:0] = ins[31:20];
+    assign zeros = 20'h00000;
+    assign ones = 20'hFFFFF;
+    yMux #(20) se(imm[31:12], zeros, ones, ins[31]);
+    assign saveImm[11:5] = ins[31:25];
+    assign saveImm[4:0] = ins[11:7];
+    yMux #(20) saveImmSe(saveImm[31:12], zeros, ones, ins[31]);
+    yMux #(32) immSelection(immOut, imm, saveImm, ins[5]);
+    assign branch[11] = ins[31];
+    assign branch[10] = ins[7];
+    assign branch[9:4] = ins[30:25];
+    assign branch[3:0] = ins[11:8];
+    yMux #(20) bra(branch[31:12], zeros, ones, ins[31]);
+    assign zerosj = 12'h000;
+    assign onesj = 12'hFFF;
+    assign jTarget[19] = ins[31];
+    assign jTarget[18:11] = ins[19:12];
+    assign jTarget[10] = ins[20];
+    assign jTarget[9:0] = ins[30:21];
+    yMux #(12) jum(jTarget[31:20], zerosj, onesj, jTarget[19]);
+
+endmodule
+
+module yEX(z, zero, rd1, rd2, imm, op, ALUSrc);
+
+    // yMux(z, a, b, c)
+    // yAlu(z, ex, a, b, op);
+
+    output [31:0] z;
+    output zero;
+    input [31:0] rd1, rd2, imm;
+    input [2:0] op;
+    input ALUSrc;
+    wire[31:0] b;
+
+    yMux #(.SIZE(32)) my_mux(b, rd2, imm, ALUSrc);
+    yAlu my_alu(z, zero, rd1, b, op);
+
+endmodule
+
